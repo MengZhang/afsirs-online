56fbcf36016f1738d2bf43de93f8484c2af27c34
MengZhang <meng.zhang15@ufl.edu>
2017-12-19 13:53:21 -0500

    Extends JSONObject for getting String value more easily

---
+++ src/main/java/org/afsirs/web/util/JSONObject.java
@@ -0,0 +1,22 @@
+package org.afsirs.web.util;
+
+import java.util.Map;
+
+/**
+ *
+ * @author Meng Zhang
+ */
+public class JSONObject extends org.json.simple.JSONObject {
+    
+    public JSONObject() {
+        super();
+    }
+    
+    public JSONObject(Map m) {
+        super(m);
+    }
+    
+    public String getOrDefault(String key, String def) {
+        return (String) super.getOrDefault(key, def);
+    }
+}
