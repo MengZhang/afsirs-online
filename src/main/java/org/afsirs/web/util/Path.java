3e82ef962b7e977aafa0ac2e4eed7d0932c302d1
MengZhang <meng.zhang15@ufl.edu>
2017-12-19 13:54:06 -0500

    Update path

--- src/main/java/org/afsirs/web/util/Path.java
+++ src/main/java/org/afsirs/web/util/Path.java
@@ -1,5 +1,7 @@
 package org.afsirs.web.util;
 
+import java.io.File;
+import java.nio.file.Paths;
 import lombok.*;
 
 public class Path {
@@ -17,8 +19,17 @@ public class Path {
             @Getter public static final String AFSIRS = PACKAGE + "/afsirs";
         }
         
-        public static class Data {
-            private static final String PACKAGE = "/" + Data.class.getSimpleName().toLowerCase();
+        public static class WaterUse {
+            private static final String PACKAGE = "/" + WaterUse.class.getSimpleName().toLowerCase() + "/permit";
+            @Getter public static final String CREATE = PACKAGE + "/create";
+            @Getter public static final String LIST = PACKAGE + "/list";
+            @Getter public static final String SEARCH = PACKAGE + "/search";
+            @Getter public static final String FIND = PACKAGE + "/find";
+            @Getter public static final String AFSIRS = PACKAGE + "/afsirs";
+        }
+        
+        public static class DataTools {
+            private static final String PACKAGE = "/" + DataTools.class.getSimpleName().toLowerCase();
             @Getter public static final String SOILMAP = PACKAGE + "/soilmap";
         }
     }
@@ -36,14 +47,31 @@ public class Path {
             public static final String AFSIRS_RESULT = PACKAGE + "/afsirs_result.ftl";
         }
         
-        public static class Data {
-            private static final String PACKAGE = Data.class.getSimpleName().toLowerCase();
+        public static class WaterUse {
+            private static final String PACKAGE = WaterUse.class.getSimpleName().toLowerCase() + "/permit";
+            public static final String CREATE = PACKAGE + "/create.ftl";
+            public static final String LIST = PACKAGE + "/list.ftl";
+            public static final String SEARCH = PACKAGE + "/search.ftl";
+            public static final String DETAIL = PACKAGE + "/detail.ftl";
+            public static final String AFSIRS = PACKAGE + "/afsirs.ftl";
+        }
+        
+        public static class DataTools {
+            private static final String PACKAGE = DataTools.class.getSimpleName().toLowerCase();
             public static final String SOILMAP = PACKAGE + "/soilmap.ftl";
         }
     }
     
     public static class Folder {
-        public final static String WORKING = "working";
+//        public final static String WORKING = "working";
+        public final static String WATER_USE_PERMIT = "Permit";
+        public static File getUserWaterUsePermitDir(String userId) {
+            File ret = Paths.get(WATER_USE_PERMIT, userId).toFile();
+            if (!ret.isDirectory()) {
+                ret.mkdirs();
+            }
+            return ret;
+        }
     }
 
 }
