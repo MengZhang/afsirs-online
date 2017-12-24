1f97c98f427c31ef9e4ae5499a1a398c68ea9c56
MengZhang <meng.zhang15@ufl.edu>
2017-12-23 22:14:28 -0500

    Update soil and weather API

--- src/main/java/org/afsirs/web/dao/bean/WaterUsePermit.java
+++ src/main/java/org/afsirs/web/dao/bean/WaterUsePermit.java
@@ -1,7 +1,14 @@
 package org.afsirs.web.dao.bean;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import javafx.util.converter.BigDecimalStringConverter;
 import lombok.Data;
+import org.afsirs.module.Soil;
 import org.afsirs.module.UserInput;
 import org.afsirs.web.util.DataUtil;
 import org.afsirs.web.util.JSONObject;
@@ -37,6 +44,25 @@ public class WaterUsePermit {
     private String et_extracted;
     private String water_table_depth;
 
+    // Soil
+    private String soil_source;
+    private LinkedHashSet<String> dbSoilNames;
+    private ArrayList<Soil> soils;
+    private String mapSoilJsonFile;
+    private String water_hold_capacity;
+    private String latitude;
+    private String longitude;
+    private String totalArea;
+
+    // Climate
+    private String et_loc;
+    private String rain_loc;
+
+    public void setDbSoilNames(String[] names) {
+        dbSoilNames = new LinkedHashSet<>();
+        dbSoilNames.addAll(Arrays.asList(names));
+    }
+
     public static WaterUsePermit readFromRequest(Request request) {
         WaterUsePermit ret = new WaterUsePermit();
         ret.setPermit_id(request.queryParams("permit_id"));
@@ -64,7 +90,47 @@ public class WaterUsePermit {
         ret.setEt_extracted(request.queryParams("et_extracted"));
         ret.setWater_table_depth(request.queryParams("water_table_depth"));
 
+        String soilSource = request.queryParams("soil_source");
+        ret.setSoil_source(soilSource);
+        ret.setWater_hold_capacity(request.queryParams("water_hold_capacity"));
+        if (soilSource.equalsIgnoreCase("DB")) {
+            ret.setDbSoilNames(request.queryParamsValues("soil_type_db"));
+            ret.setSoils(DataUtil.readSoils(ret.getDbSoilNames()));
+            ret.setTotalArea(request.queryParams("total_area"));
+        } else if (soilSource.equalsIgnoreCase("MAP")) {
+            String jsonStr = request.queryParams("soil_file_json");
+            ret.setSoils(DataUtil.toSoils(jsonStr));
+            JSONObject data = JsonUtil.parseFrom(jsonStr);
+            List<Map> asfirs = (List) data.get("asfirs");
+            if (asfirs == null) {
+                return null;
+            }
+            String longi = null;
+            String lat = null;
+//            String totArea = null;
+            for (Map node : asfirs) {
+                longi = node.get("long").toString();
+                lat = node.get("lat").toString();
+//                totArea = node.get("TotalArea").toString();
+                // TODO for multiple polygons
+            }
+            ret.setLatitude(lat);
+            ret.setLongitude(longi);
+            ret.setTotalArea(request.queryParams("total_area")); // TODO
+//            ret.setTotalArea(totArea);
+        }
+
+        ret.setEt_loc(calculateNearestStation(request.queryParams("et_loc"), "CLIMATE", ret));
+        ret.setRain_loc(calculateNearestStation(request.queryParams("rain_loc"), "RAIN", ret));
+
         return ret;
+    }
+
+    private static String calculateNearestStation(String loc, String type, WaterUsePermit permit) {
+        if ("Nearest Station".equalsIgnoreCase(loc)) {
+            loc = DataUtil.calculateNearestStation(type, permit.getLatitude(), permit.getLongitude());
+        }
+        return loc;
     }
 
     public static WaterUsePermit readFromJson(File jsonFile) {
@@ -83,7 +149,7 @@ public class WaterUsePermit {
         ret.setIrr_type(data.getOrBlank("irr_type"));
         ret.setIrr_option(data.getOrBlank("irr_option"));
         String irrDepthType = data.getOrBlank("irr_depth_type");
-        if (irrDepthType.equals("")) {
+        if (irrDepthType.isEmpty()) {
             irrDepthType = data.getOrBlank("irr_depth");
         } else {
             ret.setIrr_depth(data.getOrBlank("irr_depth"));
@@ -94,6 +160,14 @@ public class WaterUsePermit {
         ret.setSoil_surface_irr(data.getOrBlank("soil_surface_irr"));
         ret.setEt_extracted(data.getOrBlank("et_extracted"));
         ret.setWater_table_depth(data.getOrBlank("water_table_depth"));
+
+        ret.setSoil_source(data.getOrBlank("soil_source"));
+        ret.setWater_hold_capacity(data.getOrBlank("water_hold_capacity"));
+        ret.setTotalArea(data.getOrBlank("planted_area"));
+        // TODO handel soil data
+
+        ret.setEt_loc(data.getOrBlank("et_loc"));
+        ret.setRain_loc(data.getOrBlank("rain_loc"));
 
         return ret;
     }
@@ -116,6 +190,14 @@ public class WaterUsePermit {
                 DataUtil.getIRSysNameList().get(Integer.parseInt(irr_type) - 1));
 //        input.setIVERS(ir_dat);
         input.setDWT(Double.parseDouble(water_table_depth));
+
+        input.setSoilSource(soil_source);
+        input.setSoils(soils);
+        input.setWATERHOLDINGCAPACITY(water_hold_capacity);
+        input.setPlantedAcres(new BigDecimalStringConverter().fromString(totalArea).doubleValue());
+
+        input.setWeather(DataUtil.toWeather(et_loc, rain_loc));
+
         return input;
     }
 
