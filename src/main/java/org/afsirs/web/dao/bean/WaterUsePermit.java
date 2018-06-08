7e0b6e757730fe96c812debbb1afeb602ef5490b
MengZhang <meng.zhang15@ufl.edu>
2018-06-07 23:59:31 -0400

    Change the process for recover DB soil data from JSON, use local server data instead of remote db server data

--- src/main/java/org/afsirs/web/dao/bean/WaterUsePermit.java
+++ src/main/java/org/afsirs/web/dao/bean/WaterUsePermit.java
@@ -369,14 +369,20 @@ public class WaterUsePermit {
             }
         }
         ArrayList<Soil> soils = DataUtil.toSoils(data, ret.getWater_hold_capacity());
-        ret.setSoils(soils);
         if ("DB".equals(soilSource)) {
             String[] soilNames = new String[soils.size()];
             for (int i = 0; i < soils.size(); i++) {
                 soilNames[i] = soils.get(i).getSNAME();
             }
             ret.setDbSoilNames(soilNames);
+            
+            ArrayList<Soil> dbSoils = DataUtil.readSoils(ret.getDbSoilNames());
+            for (int i = 0; i < soils.size() && i < dbSoils.size(); i++) {
+                Soil dbSoil = dbSoils.get(i);
+                soils.get(i).setValues(ret.getWater_hold_capacity(), dbSoil.getWCL(), dbSoil.getWCU(), dbSoil.getDU(), new String[3]);
+            }
         }
+        ret.setSoils(soils);
 
         ret.setEt_loc(data.getOrBlank("et_loc"));
         ret.setRain_loc(data.getOrBlank("rain_loc"));
